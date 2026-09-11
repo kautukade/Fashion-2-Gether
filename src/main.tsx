ee9066be840f475e499b1da40d55954d0c9cbbce
kautukade <55291817+kautukade@users.noreply.github.com>
2026-09-11 19:52:08 +0530

    Wire product sharing and navbar visibility fixes

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,10 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import "./brand.css";
+import "./ui-fixes.css";
 import App from "./App.tsx";
+import { installProductShareHandler } from "./utils/installProductShare";
+
+installProductShareHandler();
 
 ReactDOM.createRoot(document.getElementById("root")!).render(<App />);
